6e4697c5d6d8f6457d4fe5c9de651fb17308ca39
YevhenBiriukov <yevhen.biriukov@gmail.com>
2020-08-07 20:59:26 +0300

    adding LoginPage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page = LoginPage(browser, link)
+    page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
